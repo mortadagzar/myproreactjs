d0f8322c180a184ccf6ad7d509d0397f76ecfd3d
mortada gzar <mortadagzar@gmail.com>
2018-10-16 13:43:11 -0700

    Update src/index.js

--- src/index.js
+++ src/index.js
@@ -3,19 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 
-function getGreeting(user) {
-    if (user) {
-      return <h1>Hello, {formatName(user)}!</h1>;
-    }
-    return <h1>Hello, Stranger.</h1>;
-  }
 
-  const element=<h1> {getGreeting(user)}</h1>
 
-  ReactDOM.render(
-    element,
-    document.getElementById('root')
-  );
+
+
+ReactDOM.render(
+<h1>ruhf4i4fj</h1>,
+document.getElementById('root')
+);
+
+
+ 
 
 
 // If you want your app to work offline and load faster, you can change
